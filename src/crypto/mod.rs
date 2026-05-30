fe69b2b1921b91859178f988ceadb6002e763d1a
kanrisha <dahnev@protonmail.com>
2026-05-30 16:01:24 +0200

    feat: use temp dir in decrypt fn, add docs to encrypt and decrypt fns

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -20,6 +20,17 @@ pub struct EncryptArgs {
     pub password: String,
 }
 
+/// The main function responsible for encrypting the file.
+///
+/// Processes the data in the following order:
+/// ```text
+/// Input file(s) -> Archiver -> Encoder (Compression) -> Encryptor -> BufWriter -> Output file
+/// ```
+/// Each layer implements the [`std::io::Write`] trait to pass data to the next layer.
+///
+/// # Atomicity
+/// The entire operation is atomic, meaning that a temporary file ([`utils::TempFile`]) is created to which the data is written; if an error or panic occurs, the file is deleted; otherwise, an atomic (from the file system’s perspective) rename operation takes place (which removes the temporary part from the file name).
+/// > If a situation arises where, after the entire process is complete, the temporary file remains on the disk, such a file should be considered corrupted.
 pub fn encrypt(args: EncryptArgs) -> Result<(), anyhow::Error> {
     let (temp_guard, temp_file) = utils::TempFile::create(args.output_path)?;
 
@@ -34,15 +45,16 @@ pub fn encrypt(args: EncryptArgs) -> Result<(), anyhow::Error> {
     let key = utils::hash_password(&args.password, salt)?;
 
     let encryptor = encryptor::Encryptor::new(buffered_output, &key, &nonce);
-    let compressor = zstd::Encoder::new(encryptor, 9)?;
-    let mut archiver = tar::Builder::new(compressor);
+    let encoder = zstd::Encoder::new(encryptor, 9)?;
+    // todo! encoder.multithread(...)?;
+    let mut archiver = tar::Builder::new(encoder);
 
     for path in &args.input_paths {
         archiver.append_path(path)?;
     }
 
-    let compressor = archiver.into_inner()?;
-    let encryptor = compressor.finish()?;
+    let encoder = archiver.into_inner()?;
+    let encryptor = encoder.finish()?;
     encryptor.finish()?;
 
     temp_guard.commit()?;
@@ -56,6 +68,17 @@ pub struct DecryptArgs {
     pub password: String,
 }
 
+/// The main function responsible for decrypting the file.
+///
+/// Processes the data in the following order:
+/// ```text
+/// Input file -> BufReader -> Decryptor -> Decoder (Decompression) -> Dearchiver -> Output dir
+/// ```
+/// > All decrypted files are placed in the output folder, which means that even if only a single file was encrypted, the decryption process will result in a folder containing that single file.
+///
+/// # Atomicity
+/// The entire operation is atomic, meaning that a temporary directory ([`utils::TempDir`]) is created to store the data (decrypted files); if an error or panic occurs, the directory (and the files it contains) is deleted; otherwise, an atomic (from the file system’s perspective) rename operation takes place (which removes the temporary part from the directory name).
+/// > If a situation arises where, after the entire process is complete, the temporary directory remains on the disk, such a directory (and the files it contains) should be considered corrupted.
 pub fn decrypt(args: DecryptArgs) -> Result<(), anyhow::Error> {
     let input_file = File::open(args.input_path)?;
     let mut buffered_input = BufReader::with_capacity(1024 * 1024, input_file);
@@ -68,10 +91,14 @@ pub fn decrypt(args: DecryptArgs) -> Result<(), anyhow::Error> {
     let key = utils::hash_password(&args.password, salt)?;
 
     let decryptor = decryptor::Decryptor::new(buffered_input, &key, &nonce);
-    let decompressor = zstd::Decoder::new(decryptor)?;
-    let mut dearchiver = tar::Archive::new(decompressor);
+    let decoder = zstd::Decoder::new(decryptor)?;
+    let mut dearchiver = tar::Archive::new(decoder);
 
-    dearchiver.unpack(args.output_path)?;
+    let temp_dir = utils::TempDir::create(args.output_path)?;
+
+    dearchiver.unpack(&temp_dir.temp_dir_path)?;
+
+    temp_dir.commit()?;
 
     Ok(())
 }
